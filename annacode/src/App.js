6e1e507589e9772760c74a3be5f3eb473baffc71
sachin <007mishrasachinmishra@gmail.com>
2023-03-18 19:13:38 +0530

    Added react routes

--- annacode/src/App.js
+++ annacode/src/App.js
@@ -2,7 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Homepage/Main';
 import Product_Listing from './Components/Products/Product_Listing';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path : './',
+    element: <Main/>
+  },
+  {
+    path : './products',
+    element: <Product_Listing/>
+  }
+])
 function App() {
   return (
     <>
