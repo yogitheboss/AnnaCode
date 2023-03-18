d25208b25fe2ef29754f190e9c46de40ee4f6a0d
sachin <007mishrasachinmishra@gmail.com>
2023-03-18 18:50:36 +0530

    added Product page

--- annacode/src/App.js
+++ annacode/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './Components/Homepage/Main';
+import Product_Listing from './Components/Products/Product_Listing';
 
 function App() {
   return (
     <>
-    <Main/>
+    {/* <Main/> */}
+    <Product_Listing/>
     </>
   );
 }
