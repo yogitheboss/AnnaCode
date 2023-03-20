8a422b590939c5b7453bc316851608ea81b5b7a4
sachin <007mishrasachinmishra@gmail.com>
2023-03-20 09:11:23 +0530

    .

--- annacode/src/Components/Homepage/Contact.jsx
+++ annacode/src/Components/Homepage/Contact.jsx
@@ -9,11 +9,11 @@ function Contact() {
         <div class="bg-white relative flex flex-wrap py-6 rounded shadow-md">
           <div class="lg:w-1/2 px-6">
             <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
-            <p class="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+            <p class="mt-1">Photo booth tattooed prism, <br />                portland taiyaki hoodie neutra typewriter</p>
           </div>
           <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
             <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
-            <a class="text-green-500 leading-relaxed">anna@email.com</a>
+            <a class="text-green-500 leading-relaxed">annacode@email.com</a>
             <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
             <p class="leading-relaxed">123-456-7890</p>
           </div>
