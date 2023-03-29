1ed16ab2c7d979b277d3cab6503915458c7e2bed
yogitheboss <yograjrjpt123@gmail.com>
2023-03-29 23:10:01 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 const express = require("express")
-
+const cors = require("cors")
 const app = express()
 app.use(express.json())
 require('dotenv').config()
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.send("Hello World")
 })
