0eeddfb810d9c07f75aba0026619f76efb2b562c
yogitheboss <yograjrjpt123@gmail.com>
2023-03-29 23:02:00 +0530

    made basic backend setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
