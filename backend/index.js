b72a394fbb223372991a2fee067789b4a28e1458
yogitheboss <yograjrjpt123@gmail.com>
2023-03-30 09:46:51 +0530

    made some changes

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express")
+const path = require('path');
 const cors = require("cors")
-const app = express()
-app.use(express.json())
 require('dotenv').config()
+const app = express()
+
+
+app.use(express.json())
 app.use(cors())
-
-app.use(express.static('public'))
-
+app.use(express.static(path.resolve(__dirname, '/public')));
 app.listen(4000, () => {
     console.log("Server is running on port 4000")
 })
